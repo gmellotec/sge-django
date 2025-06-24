77e4ae5ab9e1c12b8a019e182982034cf17c16bc
mindtec-guilhermem <guilherme.mello@mindtec.com.br>
2025-06-24 14:42:40 -0300

    ajustes e criacao do components charts

--- app/metrics.py
+++ app/metrics.py
@@ -1,4 +1,5 @@
-from django.db.models import Sum
+from django.db.models import Sum, F
+from django.utils import timezone
 from django.utils.formats import number_format
 from products.models import Product
 from outflows.models import Outflow
@@ -37,4 +38,40 @@ def get_sales_metrics():
         total_products_sold=total_products_sold,
         total_sales_value=__format_monetary_number(total_sales_value),
         total_sales_profit=__format_monetary_number(total_sales_profit),
+    )
+    
+
+def get_daily_sales_data():
+    today = timezone.now().date()
+    dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
+    
+    values = []
+    
+    for date in dates:
+        sales_total = Outflow.objects.filter(created_at__date=date).aggregate(total_sales=Sum(F('product__selling_price') * F('quantity')))['total_sales'] or 0
+        
+        values.append(float(sales_total))
+        
+    return dict(
+        dates=dates,
+        values=values,
+    )
+    
+    
+def get_daily_sales_quantity_data():
+    today = timezone.now().date()
+    dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
+    
+    quantities = list()
+    
+    for date in dates:
+        sales_quantity = Outflow.objects.filter(created_at__date=date).count()
+        
+        quantities.append(sales_quantity)
+        
+
+    
+    return dict(
+        dates=dates,
+        values=quantities,
     )
